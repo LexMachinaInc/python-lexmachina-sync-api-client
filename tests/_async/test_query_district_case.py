03afd09b78f3e0d149500e9cac5e912c9b437571
Jesse Carver <jcarver@lexmachina.com>
2024-02-16 11:29:33 -0700

    fix some outdated tests, add new tests for events

--- tests/_async/test_query_district_case.py
+++ tests/_async/test_query_district_case.py
@@ -1,28 +1,30 @@
-import json
 from configparser import ConfigParser
 
 import pytest
-import pprint
 
 from lexmachina import DistrictCaseQueryRequest
 from lexmachina import LexMachinaAsyncClient as LexMachinaClient
 
 
-class TestQueryDistrictCase:
-    client = LexMachinaClient("config.ini")
-    config = ConfigParser()
-    config.read("config.ini")
-    query = DistrictCaseQueryRequest()
+client = LexMachinaClient("config.ini")
+config = ConfigParser()
+config.read("config.ini")
 
 
-    @pytest.mark.asyncio
-    async def test_query_district_case_default(self):
-        response = await self.client.query_district_case(query=self.query.set_damages_minimum_amount(100))
-        assert len(response) == 5
-    @pytest.mark.asyncio
-    async def test_query_district_case_page_size_100(self):
-        response = await self.client.query_district_case(query=self.query.set_page_size(100))
-        assert len(response) == 100
+@pytest.mark.asyncio
+async def test_query_district_case_default():
+    query = DistrictCaseQueryRequest().set_damages_minimum_amount(100)
+    response = await client.query_district_case(query)
+    assert len(response) == 5
 
+@pytest.mark.asyncio
+async def test_query_district_case_page_size_100():
+    query = DistrictCaseQueryRequest().set_page_size(100).include_case_tags("Voting")
+    response = await client.query_district_case(query)
+    assert len(response) == 100
 
-
+@pytest.mark.asyncio
+async def test_query_district_case_events():
+    query = DistrictCaseQueryRequest().include_event_types("Trial")
+    response = await client.query_district_case(query)
+    assert len(response) == 5
--- tests/_sync/test_query_district_case.py
+++ tests/_sync/test_query_district_case.py
@@ -3,16 +3,22 @@ from lexmachina import LexMachinaClient
 from lexmachina import DistrictCaseQueryRequest
 
 
-class TestQueryDistrictCase:
-    client = LexMachinaClient("config.ini")
-    query = DistrictCaseQueryRequest()
-    config = ConfigParser()
-    config.read("config.ini")
 
-    def test_query_district_case_default(self):
-        response = self.client.query_district_case(query=self.query.set_damages_minimum_amount(100))
-        assert len(response) == 5
+client = LexMachinaClient("config.ini")    
+config = ConfigParser()
+config.read("config.ini")
 
-    def test_query_district_case_page_size_100(self):
-        response = self.client.query_district_case(query=self.query.set_page_size(100))
-        assert len(response) == 100
+def test_query_district_case_default():
+    query = DistrictCaseQueryRequest().set_damages_minimum_amount(100)
+    response = client.query_district_case(query)
+    assert len(response) == 5
+
+def test_query_district_case_page_size_100():
+    query = DistrictCaseQueryRequest().set_page_size(100).include_case_tags("COVID-19")
+    response = client.query_district_case(query)
+    assert len(response) == 100
+
+def test_query_district_case_events():
+    query = DistrictCaseQueryRequest().include_event_types("Trial")
+    response = client.query_district_case(query)
+    assert len(response) == 5
